da6ee248dd4d5c2af4bf64572b5a27cf4912097f
Alexey Nosenko <Onyx_2009@li.ru>
2016-11-27 22:01:13 +0300

    Initial commit

---
+++ app/src/main/java/com/example/master/anroidoneless6/TopActivity.java
@@ -0,0 +1,13 @@
+package com.example.master.anroidoneless6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top);
+    }
+}
